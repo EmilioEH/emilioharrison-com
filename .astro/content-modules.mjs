6a79dfbe48821af8677f72bf4c4d21f0f03fbead
EmilioEH <45018455+EmilioEH@users.noreply.github.com>
2025-11-28 22:17:33 -0600

    feat: add new blog post "When Your AI Breakthrough Doesn’t Save Anyone Time"

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/posts/Blog-When Your AI Breakthrough Doesn’t Save Anyone Time-20251126.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2FBlog-When+Your+AI+Breakthrough+Doesn%E2%80%99t+Save+Anyone+Time-20251126.mdoc&astroContentModuleFlag=true")],
 ["src/content/posts/testing-keystatic.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftesting-keystatic.mdoc&astroContentModuleFlag=true")]]);
 		
